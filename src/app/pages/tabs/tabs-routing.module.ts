6aa8ed2eb1d0635178d6da073c462750c84a5450
Torres92 <aleph2445@gmail.com>
2020-12-11 15:37:57 -0500

    finishing some style to proceed to map

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsPage } from '../details/details.page';
 
 import { TabsPage } from './tabs.page';
 
@@ -9,22 +10,87 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: '',
-        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+        path: 'list',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+          },
+          {
+            path: 'details',
+            loadChildren: () => import('../details/details.module').then(m => m.DetailsPageModule)
+          }
+        ]
+
       },
       {
+        path: 'map',
+        loadChildren: () => import('../map/map.module').then(m => m.MapPageModule)
+      }
+      // {
+      //   path: 'list',
+      //   children: [
+      //     {
+      //       path: '',
+      //       loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+
+      //     },
+      //     {
+      //       path: 'details',
+      //       loadChildren: () => import('../details/details.module').then(m => m.DetailsPageModule)
+
+      //     }
+      //   ]
+      // }
+      ,
+      {
         path: '',
-        redirectTo: 'home',
+        redirectTo: '/home/list',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '/home/list',
     pathMatch: 'full'
   }
 ];
+
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: TabsPage,
+//     children: [
+//       {
+//         path: 'home',
+//         children: [
+//           {
+//             path: '',
+//             loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+
+//           },
+//           {
+//             path: 'details',
+//             loadChildren: () => import('../details/details.module').then(m => m.DetailsPageModule)
+
+//           }
+//         ]
+//       },
+//       {
+//         path: '**',
+//         redirectTo: '/home',
+        
+//       }
+//     ]
+//   },
+//   {
+//     path: '',
+//     redirectTo: '/home',
+//     pathMatch: 'full'
+//   }
+// ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
